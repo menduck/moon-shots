0aa05951f3777621e4f18756feefa6cb41e43271
hyundol2 <nhs503601@gmail.com>
2023-05-23 00:08:37 +0900

    [Setup] hobbies 앱 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 INSTALLED_APPS = [
     'accounts',
+    'hobbies',
     'django_extensions',
     'storages',
     'django.contrib.admin',
